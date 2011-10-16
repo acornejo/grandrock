1ae9e66a1c4d079b04fc887aa0bb96ef81456181
Alex Cornejo <acornejo@gmail.com>
2011-10-16 03:51:16 -0400

    Initial import

---
+++ src/grandrock.cc
@@ -0,0 +1,176 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+#include <glib/gstdio.h>
+
+#include "xrandr-queyr.h"
+
+void on_item_click(GtkWidget *item, gpointer data) {
+    string *str=(string*)data;
+    if (str->find("--mode") == string::npos || \
+            gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(item)) == TRUE)
+        system(str->c_str());
+}
+
+void popupMenu(GtkStatusIcon *icon, guint button, guint32 time, gpointer data) {
+    // Query XRANDR for device info
+    vector<device_t*> devices = getdevices();
+
+    // Get list of active devices
+    vector<string> activedev;
+    for (int i=0; i<devices.size(); i++)
+        if (devices[i]->curres.size() > 0)
+            activedev.push_back(devices[i]->name);
+
+    // Genenerate popup menu
+    GtkWidget *menu = gtk_menu_new();
+    vector<device_t*>::iterator dev = devices.begin();
+    while (dev != devices.end()) {
+        GtkWidget *dev_item = gtk_check_menu_item_new_with_label((*dev)->name.c_str());
+        gtk_menu_shell_append(GTK_MENU_SHELL(menu), dev_item);
+        string prefix = "xrandr --output "+(*dev)->name;
+        if ((*dev)->curres.size() > 0) {
+            gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(dev_item), TRUE);
+
+            g_signal_connect(G_OBJECT(dev_item), "toggled",
+                    G_CALLBACK(on_item_click),
+                    new string(prefix+" --off"));
+
+            vector<string>::iterator res = (*dev)->reslist.begin();
+            GSList *group = NULL;
+            while (res != (*dev)->reslist.end()) {
+                GtkWidget *devres =
+                    gtk_radio_menu_item_new_with_label(group, (*res).c_str());
+                group = gtk_radio_menu_item_get_group(
+                        GTK_RADIO_MENU_ITEM(devres));
+                gtk_menu_shell_append(GTK_MENU_SHELL(menu), devres);
+                if (*res == (*dev)->curres)
+                    gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(devres),TRUE);
+                g_signal_connect(G_OBJECT(devres), "toggled",
+                        G_CALLBACK(on_item_click),
+                        new string(prefix + " --mode "+*res));
+                res++;
+            }
+
+            if (activedev.size() <= 1)
+                gtk_widget_set_sensitive(dev_item, FALSE);
+            else {
+                GtkWidget *pos_item = gtk_menu_item_new_with_label("Position");
+                GtkWidget  *left_item = gtk_menu_item_new_with_label("Left of");
+                GtkWidget *right_item = gtk_menu_item_new_with_label("Right of");
+                GtkWidget *above_item = gtk_menu_item_new_with_label("Above of");
+                GtkWidget *below_item = gtk_menu_item_new_with_label("Below of");
+                GtkWidget *clone_item = gtk_menu_item_new_with_label("Clone of");
+                GtkWidget *pos_menu = gtk_menu_new();
+                GtkWidget  *left_menu = gtk_menu_new();
+                GtkWidget *right_menu = gtk_menu_new();
+                GtkWidget *above_menu = gtk_menu_new();
+                GtkWidget *below_menu = gtk_menu_new();
+                GtkWidget *clone_menu = gtk_menu_new();
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(pos_item), pos_menu);
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(left_item), left_menu);
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(right_item), right_menu);
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(above_item), above_menu);
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(below_item), below_menu);
+                gtk_menu_item_set_submenu(GTK_MENU_ITEM(clone_item), clone_menu);
+                gtk_menu_shell_append(GTK_MENU_SHELL(pos_menu), left_item);
+                gtk_menu_shell_append(GTK_MENU_SHELL(pos_menu), right_item);
+                gtk_menu_shell_append(GTK_MENU_SHELL(pos_menu), above_item);
+                gtk_menu_shell_append(GTK_MENU_SHELL(pos_menu), below_item);
+                gtk_menu_shell_append(GTK_MENU_SHELL(pos_menu), clone_item);
+                for (int i=0; i<activedev.size(); i++) {
+                    if (activedev[i] != (*dev)->name) {
+                        GtkWidget *leftdev =
+                            gtk_menu_item_new_with_label(activedev[i].c_str());
+                        GtkWidget *rightdev =
+                            gtk_menu_item_new_with_label(activedev[i].c_str());
+                        GtkWidget *abovedev =
+                            gtk_menu_item_new_with_label(activedev[i].c_str());
+                        GtkWidget *belowdev =
+                            gtk_menu_item_new_with_label(activedev[i].c_str());
+                        GtkWidget *clonedev =
+                            gtk_menu_item_new_with_label(activedev[i].c_str());
+                        gtk_menu_shell_append(GTK_MENU_SHELL(left_menu),  leftdev);
+                        gtk_menu_shell_append(GTK_MENU_SHELL(right_menu), rightdev);
+                        gtk_menu_shell_append(GTK_MENU_SHELL(above_menu), abovedev);
+                        gtk_menu_shell_append(GTK_MENU_SHELL(below_menu), belowdev);
+                        gtk_menu_shell_append(GTK_MENU_SHELL(clone_menu), clonedev);
+                        g_signal_connect(G_OBJECT(leftdev), "activate",
+                                G_CALLBACK(on_item_click),
+                                new string(prefix + " --left-of "+activedev[i]));
+                        g_signal_connect(G_OBJECT(rightdev), "activate",
+                                G_CALLBACK(on_item_click),
+                                new string(prefix + " --right-of "+activedev[i]));
+                        g_signal_connect(G_OBJECT(abovedev), "activate",
+                                G_CALLBACK(on_item_click),
+                                new string(prefix + " --above "+activedev[i]));
+                        g_signal_connect(G_OBJECT(belowdev), "activate",
+                                G_CALLBACK(on_item_click),
+                                new string(prefix + " --below "+activedev[i]));
+                        g_signal_connect(G_OBJECT(clonedev), "activate",
+                                G_CALLBACK(on_item_click),
+                                new string(prefix + " --same-as "+activedev[i]));
+                    }
+                }
+                gtk_menu_shell_append(GTK_MENU_SHELL(menu), pos_item);
+            }
+        } else {
+            g_signal_connect(G_OBJECT(dev_item), "activate",
+                    G_CALLBACK(on_item_click),
+                    new string(prefix + " --auto"));
+        }
+        dev++;
+        if (dev != devices.end())
+            gtk_menu_shell_append(GTK_MENU_SHELL(menu),
+                    gtk_separator_menu_item_new());
+    }
+
+    // Free device info
+    for (int i=0; i<devices.size(); i++)
+        delete devices[i];
+    devices.clear();
+
+    gtk_widget_show_all(menu);
+
+    gtk_menu_popup(GTK_MENU(menu), NULL, NULL, gtk_status_icon_position_menu, icon, button, gtk_get_current_event_time());
+
+    g_signal_connect(G_OBJECT(menu), "delete-event", G_CALLBACK(destroyPopup), NULL);
+}
+
+void destroyPopup(GtkWidget *menu, gpointer data) {
+    printf("destroy\n");
+}
+
+static GtkStatusIcon *create_tray_icon() {
+        GtkStatusIcon *tray_icon;
+
+        tray_icon = gtk_status_icon_new();
+        g_signal_connect(G_OBJECT(tray_icon),
+                "activate",
+                 G_CALLBACK(popupMenu), NULL);
+        g_signal_connect(G_OBJECT(tray_icon),
+                 "popup-menu",
+                 G_CALLBACK(popupMenu), NULL);
+
+        GError *gerror = NULL;
+        GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("grandure-dark.png", &gerror);
+        gtk_status_icon_set_from_pixbuf(tray_icon, pixbuf);
+        gtk_status_icon_set_tooltip_text(tray_icon, "XRandr Applet");
+        gtk_status_icon_set_visible(tray_icon, TRUE);
+
+        return tray_icon;
+}
+
+int main(int argc, char **argv) {
+        GtkStatusIcon *tray_icon;
+
+        gtk_init(&argc, &argv);
+        tray_icon = create_tray_icon();
+        gtk_main();
+
+        return 0;
+}
---
+++ src/xrandr-query.h
@@ -0,0 +1,57 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct device_t {
+    string name;
+    string curres;
+    vector<string> reslist;
+};
+
+vector<device_t *> getdevices() {
+    char cbuffer[1024];
+    vector<device_t*> devices;
+
+    // Convert xrandr output into input stream
+    FILE *fp=popen("xrandr 2>&1", "r");
+    string buffer;
+    while (fgets(cbuffer, 1024, fp) != NULL)
+        buffer.append(cbuffer);
+    fclose(fp);
+    istringstream ibuffer (buffer,istringstream::in);
+
+    string line;
+    device_t *curdevice=NULL;
+    while (getline(ibuffer, line)) {
+        if (line.find(" connected") != string::npos) {
+            char name[256];
+            int resx, resy;
+            int ret=sscanf(line.c_str(), "%s connected %dx%d+", name, &resx, &resy);
+            if (ret > 0) {
+                curdevice = new device_t();
+                curdevice->name = string(name);
+                if (ret == 3) {
+                    stringstream ss;
+                    ss << resx << "x" << resy;
+                    curdevice->curres = ss.str();
+                }
+                devices.push_back(curdevice);
+            } else curdevice = NULL;
+        } else if (curdevice != NULL) {
+            int resx, resy;
+            if (sscanf(line.c_str(), "   %dx%d", &resx, &resy) == 2) {
+                stringstream ss;
+                ss << resx << "x" << resy;
+                if (find(curdevice->reslist.begin(),curdevice->reslist.end(), ss.str())== curdevice->reslist.end())
+                curdevice->reslist.push_back(ss.str());
+            }
+            else curdevice = NULL;
+        } else curdevice = NULL;
+    }
+
+    return devices;
+}
