347bc00f4d599ebda93d7b2c3b12a14b5396aa9b
Alex Cornejo <acornejo@gmail.com>
2011-10-17 14:05:30 -0400

    Cleaned up Makefiles

--- autodel.sh
+++ autodel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-make clean
+[ -f Makefile ] && make clean
+
 rm -fR configure config.status depcomp install-sh missing aclocal.m4 autom4te.cache/ config.log src/config.{h,h.in}
 
 find . \( -name Makefile -o -name Makefile.in -o -name "stamp-h*" \) -exec rm -f {} \;
