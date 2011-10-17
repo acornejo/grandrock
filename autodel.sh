7f644f86d7a823d4e44a44c6921035621408c12b
Alex Cornejo <acornejo@gmail.com>
2011-10-17 15:36:47 -0400

    Delete files generated by debuild.

--- autodel.sh
+++ autodel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ -f Makefile ] && make clean
 
-rm -fR configure config.status depcomp install-sh missing aclocal.m4 autom4te.cache/ config.log src/config.{h,h.in}
+rm -fR configure config.status depcomp install-sh missing aclocal.m4 autom4te.cache/ config.log src/config.{h,h.in} debian/grandrock* debian/autoreconf* debian/files
 
 find . \( -name Makefile -o -name Makefile.in -o -name "stamp-h*" \) -exec rm -f {} \;
